3745e0bd49d13361735a6db283cfa15dce49544b
Juan Esteban Lopez <jelopezta@gmail.com>
2022-02-17 12:40:00 -0700

    basic function with string return

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-var deckSize int
-
 func main() {
-	// var card string = "Ace of spades"
-	card := "Ace of spades"
-	card = "Five of Diamonds"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
