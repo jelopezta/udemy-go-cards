e68973d8ac52ba81cd3e471c24ac6eb704aa87f5
Juan Esteban Lopez <jelopezta@gmail.com>
2022-02-17 12:36:45 -0700

    basic variable initialization and declaration

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var deckSize int
+
+func main() {
+	// var card string = "Ace of spades"
+	card := "Ace of spades"
+	card = "Five of Diamonds"
+	fmt.Println(card)
+}
